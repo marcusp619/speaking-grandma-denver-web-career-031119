3fcf83f7c2f7921928d3dd2411b15d0ace991045
Mark Pearyer <marcusp619@gmail.com>
2019-04-24 17:04:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(say) 
-  if say === say.
+  if say == say.upcase
+    puts 'NO, NOT SINCE 1938!'
+  elsif 
   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
